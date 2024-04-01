9fdc9177075dee2bb87e0625311d5e7d14cdd6ce
Anton Boldyrev <boldyrev.webdev@gmail.com>
2024-04-01 11:50:48 +0500

    dev: add customer Home Page

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,48 @@
+import { formatCurrency } from "@/lib/formatters"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card"
+import { Button } from "./ui/button"
+import Link from "next/link"
+import Image from "next/image"
+
+type ProductCardProps = {
+  name: string
+  description: string
+  id: string
+  priceInCents: number
+  imagePath: string
+}
+
+export default function ProductCard({
+  name,
+  description,
+  id,
+  priceInCents,
+  imagePath,
+}: ProductCardProps) {
+  return (
+    <Card className="flex overflow-hidden flex-col">
+      <div className="relative w-full h-auto aspect-video">
+        <Image src={imagePath} fill alt={name} />
+      </div>
+      <CardHeader>
+        <CardTitle>{name}</CardTitle>
+        <CardDescription>{formatCurrency(priceInCents / 100)}</CardDescription>
+      </CardHeader>
+      <CardContent className="flex-grow">
+        <p className="line-clamp-4">{description}</p>
+      </CardContent>
+      <CardFooter>
+        <Button asChild size="lg" className="w-full">
+          <Link href={`/products/${id}/purchase`}>Purchase</Link>
+        </Button>
+      </CardFooter>
+    </Card>
+  )
+}
