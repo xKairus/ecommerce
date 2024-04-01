6fcad3eb20d8e26de03098176b81fd4fb7f21496
Anton Boldyrev <boldyrev.webdev@gmail.com>
2024-04-01 14:54:47 +0500

    dev: add cache.ts

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -4,6 +4,7 @@ import db from "@/db/db"
 import { z } from "zod"
 import fs from "fs/promises"
 import { notFound, redirect } from "next/navigation"
+import { revalidatePath } from "next/cache"
 
 const fileSchema = z.instanceof(File, { message: "Required" })
 const imageSchema = fileSchema.refine(
@@ -47,6 +48,9 @@ export async function addProduct(prevState: unknown, formData: FormData) {
       imagePath,
     },
   })
+
+  revalidatePath("/")
+  revalidatePath("/products")
 
   redirect("/admin/products")
 }
@@ -99,6 +103,9 @@ export async function updateProduct(
     },
   })
 
+  revalidatePath("/")
+  revalidatePath("/products")
+
   redirect("/admin/products")
 }
 
@@ -107,6 +114,9 @@ export async function toggleProductAvailability(
   isAvailableForPurchase: boolean
 ) {
   await db.product.update({ where: { id }, data: { isAvailableForPurchase } })
+
+  revalidatePath("/")
+  revalidatePath("/products")
 }
 
 export async function deleteProduct(id: string) {
@@ -115,4 +125,7 @@ export async function deleteProduct(id: string) {
 
   await fs.unlink(product.filePath)
   await fs.unlink(`public/${product.imagePath}`)
+
+  revalidatePath("/")
+  revalidatePath("/products")
 }
