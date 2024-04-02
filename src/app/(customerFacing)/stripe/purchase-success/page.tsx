4d0a218a819e7714a2f2509154602fb88cc97e2a
Anton Boldyrev <boldyrev.webdev@gmail.com>
2024-04-02 10:14:49 +0500

    dev: download ordered file functionality

--- src/app/(customerFacing)/stripe/purchase-success/page.tsx
+++ src/app/(customerFacing)/stripe/purchase-success/page.tsx
@@ -49,7 +49,13 @@ export default async function SuccessPage({
           </div>
           <Button className="mt-4" size="lg" asChild>
             {isSuccess ? (
-              <a >Download</a>
+              <a
+                href={`/products/download/${await createDownloadVerification(
+                  product.id
+                )}`}
+              >
+                Download
+              </a>
             ) : (
               <Link href={`/products/${product.id}/purchase`}>Try again</Link>
             )}
@@ -58,4 +64,15 @@ export default async function SuccessPage({
       </div>
     </div>
   )
+}
+
+async function createDownloadVerification(productId: string) {
+  return (
+    await db.downloadVerification.create({
+      data: {
+        productId,
+        expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24),
+      },
+    })
+  ).id
 }
